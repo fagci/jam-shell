d39b5dd4e86638a9a0c61ff92a626d26f3506c2e
Mikhail Yudin <fagci.nsk@gmail.com>
2021-12-27 03:01:09 +0700

    refactor

--- jam-shell.sh
+++ jam-shell.sh
@@ -8,32 +8,39 @@ shopt -s globstar
 
 mkdir -p "$OUTPUT"
 
-function render() {
-    # TODO: smart eval (keep indentation)
-    eval echo '"'"$(sed 's_"_\\"_g')"'"'
-}
-
-function render_template() {
+function normalize() {
     # Make an empty config or format existing config block
-    local content=$(awk '
-        NR==1 && /^---/ { has_config = 1; next }
+    awk 'NR==1 && /^---/ { has_config = 1; next }
+        NR!=1 && has_config && /^\s*[a-zA-Z0-9_]+=/ {printf "local "}
         NR!=1 && has_config && /^---/ { has_config = 0; printf "@@@"; next}
         NR==1 && !has_config { printf "@@@" }
-        1
-    ')
+        1' | sed 's_"_\\"_g' | sed -E 's/\{\s*(\S+)\s*\}/$\1/g'
+}
 
-    local commands="${content%@@@*}"
-    local content="${content#*@@@}"
+function eval_text() {
+    eval echo '"'"$1"'"'
+}
 
-    eval "$commands"
+function render() {
+    local content_file="$1"
+    local contents="$(normalize < "$content_file")"
 
-    local template="${template:-base.html}"
+    local content_cfg="${contents%@@@*}"
 
-    sed -E 's/\{\s*(\S+)\s*\}/$\1/g' "${TEMPLATES}/${template}" \
-        | render | render # render template, then render content
+    # choose template
+    eval "$(sed -n '/template=/p' <<< "$content_cfg")"
+
+    local _template=$(normalize < "${TEMPLATES}/${template:-base.html}")
+
+    eval "${_template%@@@*}"
+    eval "${content_cfg}"
+
+    # TODO: smart eval (keep indentation)
+    local content="$(eval_text "${contents#*@@@}")"
+    eval_text "${_template#*@@@}"
 }
 
 for content_file in "$CONTENT"/**/*; do
-    render_template < "$content_file" > "${OUTPUT}/${content_file#*${CONTENT}}"
+    render "$content_file" > "${OUTPUT}/${content_file#*${CONTENT}}"
 done
 
